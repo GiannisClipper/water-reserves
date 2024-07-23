41a5df14655de434b62e87c4dc6c33bc90de087d
Giannis Clipper <giannisclipper@gmail.com>
2024-07-23 12:06:13 +0300

    Setup settings (including db credentials).

--- src/main.py
+++ src/main.py
@@ -4,6 +4,10 @@ from src.routers import reservoirs as reservoirs_router
 from src.routers import factories as factories_router
 from src.routers import locations as locations_router
 
+from .settings import get_settings
+
+settings = get_settings()
+
 app = FastAPI()
 
 app.include_router( reservoirs_router.router )
@@ -12,4 +16,6 @@ app.include_router( locations_router.router )
 
 @app.get( '/', description="This is the home endpoint." )
 async def home():
+    print( repr( settings ) )
+    settings.db_name = 'already printed'
     return { "message": "Water-reserves back-end is up and running..." }
