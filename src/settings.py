9e39bad65ccc3f5e55faf20bb886ea4531473596
Giannis Clipper <giannisclipper@gmail.com>
2024-07-27 13:00:08 +0300

    Update settings (config env_file).

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 from functools import lru_cache
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings( BaseSettings ):
     db_host: str = ""
@@ -9,8 +9,10 @@ class Settings( BaseSettings ):
     db_user: str = ""
     db_password: str = ""
 
-    class Config:
-        env_file = "resources/.env"
+    model_config = SettingsConfigDict( env_file='resources/.env', env_file_encoding='utf-8' )
+
+    # class Config:
+    #     env_file = "resources/.env"
 
 @lru_cache() # due to @lru_cache() will return a singleton object
 def get_settings():
