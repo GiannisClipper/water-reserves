ef1a8889e28483b098d4bae08f0440a82e07c717
Giannis Clipper <giannisclipper@gmail.com>
2024-07-29 15:02:39 +0300

    Refactor helpers/time.py

--- src/helpers/time.py
+++ src/helpers/time.py
@@ -1,31 +1,71 @@
-def is_year( year: str ):
+# to check formats
 
-    try:
-        return int( year ) >=1900 and int( year ) <= 2100
-    except Exception:
+def has_year_format( val: str ):
+    return len( val ) == 4 and val.isdigit()
+
+def has_month_format( val: str ):
+    return len( val ) == 2 and val.isdigit()
+
+def has_day_format( val: str ):
+    return len( val ) == 2 and val.isdigit()
+
+def has_month_day_format( val: str ):
+    arr = val.split( '-' )
+    if len( arr ) != 2:
         return False
 
+    month, day = arr
+    return has_month_format( month ) and has_day_format( day )
+
+def has_date_format( val: str ):
+
+    arr = val.split( '-' )
+    if len( arr ) != 3:
+        return False
+
+    year, month, day = arr
+    return has_year_format( year ) and has_month_format( month ) and has_day_format( day )
+
+
+# to check values
+
+def is_year( year: str ):
+
+    if not has_year_format( year ):
+        raise ValueError( 'Invalid year format.' )
+
+    return int( year ) >=1900 and int( year ) <= 2100
 
 def is_leap( year: str ):
 
-    if is_year( year ):
-        year = int( year )
-        if year % 400 == 0:
-            return True
-        if year % 100 == 0:
-            return False
-        if year % 4 == 0:
-            return True
+    if not is_year( year ):
+        raise ValueError( 'Invalid year value.' )
+
+    year = int( year )
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
         return False
-
-    raise ValueError( 'Invalid year value.' )
-
+    if year % 4 == 0:
+        return True
+    return False
 
 def is_month( month: str ):
+
+    if not has_month_format( month ):
+        raise ValueError( 'Invalid month format.' )
+
     return month in ( '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12' )
 
 
-def month_days( month: str, year: str | None = None ):
+def get_month_days( month: str, year: str | None = None ):
+
+    if not is_month( month ):
+        raise ValueError( 'Invalid month value.' )
+
+    if year and not is_year( year ):
+        raise ValueError( 'Invalid year value.' )
+
     days = [ 
         '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
         '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
@@ -50,13 +90,16 @@ def month_days( month: str, year: str | None = None ):
             days.pop()
             return days
 
-    raise ValueError( 'Invalid month value' )
-
-
 def is_day( day: str, month: str, year: str | None = None ):
 
-    if day in month_days( '01' ):
-        return day in month_days( month, year )
+    if not has_day_format( day ):
+        raise ValueError( 'Invalid day format.' )
 
-    raise ValueError( 'Invalid day value.' )
+    if not is_month( month ):
+        raise ValueError( 'Invalid month value.' )
+
+    if year and not is_year( year ):
+        raise ValueError( 'Invalid year value.' )
+
+    return day in get_month_days( month, year )
 
--- tests/helpers/test_time.py
+++ tests/helpers/test_time.py
@@ -1,16 +1,84 @@
-from src.helpers.time import is_year, is_leap, is_month, month_days, is_day
+from src.helpers.time import has_year_format, has_month_format, has_day_format, has_month_day_format, has_date_format
+from src.helpers.time import is_year, is_leap, is_month, get_month_days, is_day
+
+# has_year_format
+
+def test_has_year_format_invalid_text():
+    assert has_year_format( '123' ) == False
+    assert has_year_format( '12345' ) == False
+    assert has_year_format( '4chr' ) == False
+
+def test_has_year_format_valid_text():
+    assert has_year_format( '1234' ) == True
+    assert has_year_format( '2024' ) == True
+
+# has_month_format
+
+def test_has_month_format_invalid_text():
+    assert has_month_format( '1' ) == False
+    assert has_month_format( '123' ) == False
+    assert has_month_format( '2c' ) == False
+
+def test_has_month_format_valid_text():
+    assert has_month_format( '00' ) == True
+    assert has_month_format( '99' ) == True
+    assert has_month_format( '01' ) == True
+
+# has_day_format
+
+def test_has_day_format_invalid_text():
+    assert has_day_format( '1' ) == False
+    assert has_day_format( '123' ) == False
+    assert has_day_format( '2c' ) == False
+
+def test_has_day_format_valid_text():
+    assert has_day_format( '99' ) == True
+    assert has_day_format( '01' ) == True
+
+# has_month_day_format
+
+def test_has_month_day_format_invalid_text():
+    assert has_month_day_format( '01' ) == False
+    assert has_month_day_format( '2023-01-01' ) == False
+    assert has_month_day_format( '2024-01' ) == False
+    assert has_month_day_format( 'Jan-01' ) == False
+    assert has_month_day_format( 'Ja-01' ) == False
+
+def test_has_month_day_format_valid_text():
+    assert has_month_day_format( '99-99' ) == True
+    assert has_month_day_format( '01-31' ) == True
+
+# has_date_format
+
+def test_has_date_format_invalid_text():
+    assert has_date_format( '01' ) == False
+    assert has_date_format( '2024-01' ) == False
+    assert has_date_format( '2023-jan-01' ) == False
+    assert has_date_format( '2023-jan-01' ) == False
+    assert has_date_format( '2023-01-1st-day' ) == False
+
+def test_has_date_format_valid_text():
+    assert has_date_format( '9999-99-99' ) == True
+    assert has_date_format( '2024-01-31' ) == True
 
 # is_year
 
+def test_is_year_valueerror():
+    try:
+        is_year( 'year' )
+        assert False
+    except ValueError:
+        assert True
+
 def test_is_year_invalid_text():
-    assert is_year( 'four' ) == False
+    assert is_year( '1234' ) == False
+    assert is_year( '1899' ) == False
+    assert is_year( '2101' ) == False
 
-def test_is_year_invalid_range():
-    assert is_year( '01' ) == False
-    assert is_year( '20240' ) == False
-
-def test_is_year_valid():
+def test_is_year_valid_text():
+    assert is_year( '1900' ) == True
     assert is_year( '2024' ) == True
+    assert is_year( '2100' ) == True
 
 # is_leap
 
@@ -33,57 +101,75 @@ def test_is_leap_true():
 
 # is_month
 
+def test_is_month_valueerror():
+    try:
+        is_month( 'month' )
+        assert False
+    except ValueError:
+        assert True
+
 def test_is_month_false():
-    assert is_month( 'blah.. blah..' ) == False
-    assert is_month( 'jan' ) == False
     assert is_month( '00' ) == False
-    assert is_month( '1' ) == False
     assert is_month( '13' ) == False
 
 def test_is_month_true():
     assert is_month( '01' ) == True
     assert is_month( '12' ) == True
 
-# month_days
+# get_month_days
 
-def test_month_days_valueerror():
+def test_get_month_days_valueerror():
     try:
-        month_days( 'jan' )
+        get_month_days( 'jan' )
         assert False
     except ValueError:
         assert True
 
-def test_month_days():
-    assert month_days( '01' )[ -1 ] == '31'
-    assert month_days( '02', '2023' )[ -1 ] == '28'
-    assert month_days( '02', '2024' )[ -1 ] == '29'
-    assert month_days( '02' )[ -1 ] == '29'
-    assert month_days( '03' )[ -1 ] == '31'
-    assert month_days( '04' )[ -1 ] == '30'
-    assert month_days( '05' )[ -1 ] == '31'
-    assert month_days( '06' )[ -1 ] == '30'
-    assert month_days( '07' )[ -1 ] == '31'
-    assert month_days( '08' )[ -1 ] == '31'
-    assert month_days( '09' )[ -1 ] == '30'
-    assert month_days( '10' )[ -1 ] == '31'
-    assert month_days( '11' )[ -1 ] == '30'
-    assert month_days( '12' )[ -1 ] == '31'
+def test_month_days_results():
+    assert get_month_days( '01' )[ -1 ] == '31'
+    assert get_month_days( '02', '2023' )[ -1 ] == '28'
+    assert get_month_days( '02', '2024' )[ -1 ] == '29'
+    assert get_month_days( '02' )[ -1 ] == '29'
+    assert get_month_days( '03' )[ -1 ] == '31'
+    assert get_month_days( '04' )[ -1 ] == '30'
+    assert get_month_days( '05' )[ -1 ] == '31'
+    assert get_month_days( '06' )[ -1 ] == '30'
+    assert get_month_days( '07' )[ -1 ] == '31'
+    assert get_month_days( '08' )[ -1 ] == '31'
+    assert get_month_days( '09' )[ -1 ] == '30'
+    assert get_month_days( '10' )[ -1 ] == '31'
+    assert get_month_days( '11' )[ -1 ] == '30'
+    assert get_month_days( '12' )[ -1 ] == '31'
 
 # is_day
 
-def test_is_day_valueerror():
+def test_is_day_invalid_day_format():
     try:
         is_day( '1', '12', '2023' )
         assert False
-    except ValueError:
-        assert True
+    except ValueError as e:
+        assert 'Invalid day format.' in repr( e )
 
-def test_is_day_false():
+def test_is_day_invalid_month_value():
+    try:
+        is_day( '01', '13', '2023' )
+        assert False
+    except ValueError as e:
+        assert 'Invalid month value.' in repr( e )
+
+def test_is_day_invalid_year_value():
+    try:
+        is_day( '01', '12', '9999' )
+        assert False
+    except ValueError as e:
+        assert 'Invalid year value.' in repr( e )
+
+def test_is_day_invalid_values():
     assert is_day( '29', '02', '2023' ) == False
     assert is_day( '30', '02', '2024' ) == False
     assert is_day( '31', '04', '2024' ) == False
 
-def test_is_day_true():
+def test_is_day_valid_values():
     assert is_day( '28', '02', '2024' ) == True
     assert is_day( '29', '02', '2024' ) == True
     assert is_day( '30', '04', '2024' ) == True
