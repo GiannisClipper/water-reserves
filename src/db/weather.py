fec78650ab9301ca7341300f448ff4c05511bfb1
Giannis Clipper <giannisclipper@gmail.com>
2024-07-31 13:34:07 +0300

    Refactor weather db queries (add class QueryMaker).

--- src/db/weather.py
+++ src/db/weather.py
@@ -4,7 +4,7 @@ from psycopg.rows import class_row
 from src.db import pool
 
 from src.helpers.time import get_first_date, get_last_date
-from src.helpers.text import get_query_headers
+from src.helpers.text import set_indentation, get_query_headers
 
 # result is possible to have a large amount of rows
 # so, in order to reduce the response size, is preferred 
@@ -17,133 +17,228 @@ from src.helpers.text import get_query_headers
 #     date: str
 #     precipitation_sum: real
 
-def create_base_query( time_range, location_filter, interval_filter ):
+class QueryMaker:
 
-    from_time, to_time = time_range if time_range else ( None, None )
-    from_date = get_first_date( from_time )
-    to_date = get_last_date( to_time )
+    def __init__(
+        self,
+        time_range: str | None, 
+        location_filter: str | None,
+        interval_filter: str | None,
+        location_aggregation: str | None,
+        time_aggregation: str | None,
+        year_start: str | None
+    ):
+        self.time_range = time_range
+        self.location_filter = location_filter
+        self.interval_filter = interval_filter
+        self.location_aggregation = location_aggregation
+        self.time_aggregation = time_aggregation
+        self.year_start = year_start
+        self.query = None
+    
+        aliases = [ 'c', 'b', 'a' ]
+        self.__create_base_query()
 
-    where_clause = []
+        if self.location_aggregation:
+            query = self.__expand_query_with_location_aggregation( aliases.pop() )
 
-    if from_date:
-        where_clause.append( f"date>='{from_date}'" )
+        if self.time_aggregation and self.time_aggregation[ 0 ] == 'month':
+            self.__expand_query_with_month_aggregation( aliases.pop() )
 
-    if to_date:
-        where_clause.append( f"date<='{to_date}'" )
+        if self.time_aggregation and self.time_aggregation[ 0 ] == 'year':
+            if not self.year_start:
+                self.__expand_query_with_year_aggregation( aliases.pop() )
+            else:
+                self.__expand_query_with_custom_year_header( aliases.pop() )
+                self.__expand_query_with_custom_year_aggregation( aliases.pop() )
 
-    if location_filter:
-        where_clause.append( f"location_id IN ({location_filter})" )
-
-    if interval_filter:
-        op = 'AND' if interval_filter[ 0 ] <= interval_filter[ 1 ] else 'OR'
-        where_clause.append( f"(SUBSTR(date,6,5)>='{interval_filter[ 0 ]}' {op} SUBSTR(date,6,5)<='{interval_filter[ 1 ]}')" )
-
-    if len( where_clause ) > 0:
-        where_clause = ' AND '.join( where_clause )
-        where_clause = f'WHERE {where_clause}'
-    else:
-        where_clause = ''
-
-    return f'''
-        SELECT id, date, location_id, precipitation_sum 
-        FROM weather
-        {where_clause}
-    '''
-
-
-def expand_query_with_location_aggregation( query ):
-
-    return f'''
-        SELECT 
-        a.date AS date, '' AS location_id, SUM(a.precipitation_sum) AS precipitation_sum 
-        FROM (
-        {query}
-        ) a 
-        GROUP BY 
-        a.date
-    '''
+        self.__expand_query_with_order()
 
 
-def expand_query_with_month_aggregation( query, aggregation_method ):
-
-    # use ::numeric to handle => 
-    # psycopg.errors.UndefinedFunction: function round(double precision, integer) does not exist
-    # LINE 6: ROUND(AVG(b.precipitation_sum),2) AS precipitation_sum
-    # https://stackoverflow.com/questions/58731907/error-function-rounddouble-precision-integer-does-not-exist
-
-    aggregation_method = "ROUND(AVG(b.precipitation_sum::numeric),2)" if aggregation_method == 'avg' else "SUM(b.precipitation_sum)"
-
-    return f'''
-        SELECT 
-        SUBSTR(b.date,1,7) AS month, 
-        b.location_id AS location_id, 
-        {aggregation_method} AS precipitation_sum 
-        FROM (
-        {query}
-        ) b 
-        GROUP BY 
-        SUBSTR(b.date,1,7), 
-        b.location_id
-    '''
-
-def expand_query_with_year_aggregation( query, aggregation_method ):
-
-    aggregation_method = "ROUND(AVG(b.precipitation_sum::numeric),2)" if aggregation_method == 'avg' else "SUM(b.precipitation_sum)"
-
-    return f'''
-        SELECT
-        SUBSTR(b.date,1,4) AS year, 
-        b.location_id AS location_id, 
-        {aggregation_method} AS precipitation_sum 
-        FROM (
-        {query}
-        ) b 
-        GROUP BY 
-        SUBSTR(b.date,1,4), 
-        b.location_id
-    '''
+    def get_query( self ):
+        return self.query
 
 
-def expand_query_with_custom_year_aggregation( query, year_start, aggregation_method ):
+    def __create_base_query( self ):
 
-    custom_year = f'''
-        CASE WHEN SUBSTR(b.date,6,5)>='{year_start}'
-        THEN SUBSTR(b.date,1,4) || '-' || CAST(SUBSTR(b.date,1,4) AS INTEGER)+1
-        ELSE CAST(SUBSTR(b.date,1,4) AS INTEGER)-1 || '-' || SUBSTR(b.date,1,4) 
-        END'''
+        from_time, to_time = self.time_range if self.time_range else ( None, None )
+        from_date = get_first_date( from_time )
+        to_date = get_last_date( to_time )
 
-    query = f'''
-        SELECT 
-        {custom_year} AS custom_year,
-        b.location_id AS location_id, b.precipitation_sum AS precipitation_sum 
-        FROM (
-        {query}
-        ) b
-    '''
+        where_clause = []
 
-    aggregation_method = "ROUND(AVG(c.precipitation_sum::numeric),2)" if aggregation_method == 'avg' else "SUM(c.precipitation_sum)"
+        if from_date:
+            where_clause.append( f"date>='{from_date}'" )
 
-    return f'''
-        SELECT 
-        c.custom_year AS custom_year, 
-        c.location_id AS location_id, 
-        {aggregation_method} AS precipitation_sum 
-        FROM (
-        {query}
-        ) c
-        GROUP BY 
-        c.custom_year, 
-        c.location_id
-    '''
+        if to_date:
+            where_clause.append( f"date<='{to_date}'" )
+
+        if self.location_filter:
+            where_clause.append( f"location_id IN ({self.location_filter})" )
+
+        if self.interval_filter:
+            op = 'AND' if self.interval_filter[ 0 ] <= self.interval_filter[ 1 ] else 'OR'
+            text = f"(SUBSTR(date,6,5)>='{self.interval_filter[ 0 ]}' {op} SUBSTR(date,6,5)<='{self.interval_filter[ 1 ]}')"
+            where_clause.append( text )
+
+        if len( where_clause ) > 0:
+            where_clause = ' AND '.join( where_clause )
+            where_clause = f'WHERE {where_clause}'
+        else:
+            where_clause = ''
+
+        self.query = f'''
+            SELECT id, date, location_id, precipitation_sum 
+            FROM weather
+            {where_clause}'''
 
 
-def expand_query_with_order( query, order ):
+    def __expand_query_with_location_aggregation(self,  alias ):
 
-    return f'''
-        {query}
-        ORDER BY 
-        {order}
-    '''
+        self.query = f'''
+            SELECT 
+            {alias}.date AS date, SUM({alias}.precipitation_sum) AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias} 
+            GROUP BY 
+            {alias}.date'''
+
+
+    def __expand_query_with_month_aggregation( self, alias ):
+
+        method = self.time_aggregation[ 1 ]
+        precipitation_sum = f"ROUND(AVG({alias}.precipitation_sum::numeric),2)" if method == 'avg' else f"SUM({alias}.precipitation_sum)"
+
+        # ::numeric is used to handle => 
+        # psycopg.errors.UndefinedFunction: function round(double precision, integer) does not exist
+        # LINE 6: ROUND(AVG(b.precipitation_sum),2) AS precipitation_sum
+        # https://stackoverflow.com/questions/58731907/error-function-rounddouble-precision-integer-does-not-exist
+
+        if self.location_aggregation:
+            self.query = f'''
+            SELECT 
+            SUBSTR({alias}.date,1,7) AS month, 
+            {precipitation_sum} AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias} 
+            GROUP BY 
+            SUBSTR({alias}.date,1,7)'''
+
+        else:
+            self.query = f'''
+            SELECT 
+            SUBSTR({alias}.date,1,7) AS month, 
+            {alias}.location_id AS location_id, 
+            {precipitation_sum} AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias} 
+            GROUP BY 
+            SUBSTR({alias}.date,1,7), 
+            {alias}.location_id'''
+
+
+    def __expand_query_with_year_aggregation( self, alias ):
+
+        method = self.time_aggregation[ 1 ]
+        precipitation_sum = f"ROUND(AVG({alias}.precipitation_sum::numeric),2)" if method == 'avg' else f"SUM({alias}.precipitation_sum)"
+
+        if self.location_aggregation:
+            self.query = f'''
+            SELECT
+            SUBSTR({alias}.date,1,4) AS year, 
+            {precipitation_sum} AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias} 
+            GROUP BY 
+            SUBSTR({alias}.date,1,4)'''
+
+        else:
+            self.query = f'''
+            SELECT
+            SUBSTR({alias}.date,1,4) AS year, 
+            {alias}.location_id AS location_id, 
+            {precipitation_sum} AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias} 
+            GROUP BY 
+            SUBSTR({alias}.date,1,4), 
+            {alias}.location_id'''
+
+
+    def __expand_query_with_custom_year_header( self, alias ):
+
+        custom_year = f'''
+            CASE WHEN SUBSTR({alias}.date,6,5)>='{self.year_start}'
+            THEN SUBSTR({alias}.date,1,4) || '-' || CAST(SUBSTR({alias}.date,1,4) AS INTEGER)+1
+            ELSE CAST(SUBSTR({alias}.date,1,4) AS INTEGER)-1 || '-' || SUBSTR({alias}.date,1,4) 
+            END'''
+
+        if self.location_aggregation:
+            self.query = f'''
+            SELECT 
+            {custom_year} AS custom_year,
+            {alias}.precipitation_sum AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias}'''
+        else:
+            self.query = f'''
+            SELECT 
+            {custom_year} AS custom_year,
+            {alias}.location_id AS location_id, 
+            {alias}.precipitation_sum AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias}'''
+
+
+    def __expand_query_with_custom_year_aggregation( self, alias ):
+
+        method = self.time_aggregation[ 1 ]
+        precipitation_sum = f"ROUND(AVG({alias}.precipitation_sum),2)" if method == 'avg' else f"SUM({alias}.precipitation_sum)"
+
+        if self.location_aggregation:
+            self.query = f'''
+            SELECT 
+            {alias}.custom_year AS custom_year, 
+            {precipitation_sum} AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias}
+            GROUP BY 
+            {alias}.custom_year'''
+
+        else:
+            self.query = f'''
+            SELECT 
+            {alias}.custom_year AS custom_year, 
+            {alias}.location_id AS location_id, 
+            {precipitation_sum} AS precipitation_sum 
+            FROM (
+            {set_indentation( 4, self.query )}
+            ) {alias}
+            GROUP BY 
+            {alias}.custom_year, 
+            {alias}.location_id'''
+
+
+    def __expand_query_with_order( self ):
+
+        headers = get_query_headers( self.query )
+        order = headers[ 0 ]
+        if not self.location_aggregation:
+            order = f"{order},{headers[ 1 ]}"
+
+        self.query =f'''
+            {self.query}
+            ORDER BY 
+            {order}
+        '''
 
 
 async def select_all( 
@@ -155,41 +250,15 @@ async def select_all(
     year_start: str | None
 ):
 
-    query = create_base_query( time_range, location_filter, interval_filter )
-    # print( 'base_query:', query )
-    order = 'date,location_id'
+    query = QueryMaker( 
+        time_range, location_filter, interval_filter, 
+        location_aggregation, time_aggregation, year_start 
+    ).get_query()
 
-    if location_aggregation:
-        query = expand_query_with_location_aggregation( query )
-        # print( 'with_location_aggregation:', query )
-
-    if time_aggregation and time_aggregation[ 0 ] == 'month':
-
-        aggregation_method = time_aggregation[ 1 ]
-
-        query = expand_query_with_month_aggregation( query, aggregation_method )
-        # print( 'with_month_aggregation:', query )
-        order = 'month,location_id'
-
-    if time_aggregation and time_aggregation[ 0 ] == 'year':
-
-        aggregation_method = time_aggregation[ 1 ]
-
-        if not year_start:
-            query = expand_query_with_year_aggregation( query, aggregation_method )
-            # print( 'with_year_aggregation:', query )
-            order = 'year,location_id'
-
-        else:
-            query = expand_query_with_custom_year_aggregation( query, year_start, aggregation_method )
-            # print( 'with_custom_year_aggregation:', query )
-            order = 'custom_year,location_id'
-
-    query = expand_query_with_order( query, order )
     print( 'query:', query )
 
     headers = get_query_headers( query )
-    data =[]
+    data = []
 
     async with pool.connection() as conn, conn.cursor() as cur:
         await cur.execute( query )
