790cc0b57f07e474aee4422a732ef739398bd60b
Giannis Clipper <giannisclipper@gmail.com>
2024-08-25 17:20:36 +0300

    Working on savings page (helpers/Requests).

--- src/app/savings/DataSection.tsx
+++ src/app/savings/DataSection.tsx
@@ -2,10 +2,8 @@ import { Suspense } from "react";
 import ChartSection from "./ChartSection";
 import ListSection from "./ListSection";
 import type { SearchParamsType } from "@/types/searchParams";
-import type { SavingsQueryParamsType } from "@/types/queryParams";
 import type { RequestResultType } from "@/types/requestResult";
-import { parseSavingsQueryParams } from "@/helpers/params";
-import { REST_API_BASE_URL } from '../settings';
+import { SavingsRequest } from "@/helpers/Requests";
 
 type PropsType = { searchParams: SearchParamsType }
 
@@ -13,13 +11,11 @@ const DataSection = async ( { searchParams }: PropsType ) => {
 
     let result: RequestResultType | null = null;
 
-    const queryParams: SavingsQueryParamsType = parseSavingsQueryParams( searchParams );
+    const savingsRequest = new SavingsRequest( searchParams );
 
-    if ( queryParams.time_range ) {
-        const queryParamsString: string = Object.entries( queryParams ).map( entry => `${entry[ 0 ]}=${entry[ 1 ]}` ).join( '&' );
-        const url: string = `${REST_API_BASE_URL}/savings?${queryParamsString}`;
-        console.log( url );
-        const response = await fetch( url );
+    if ( savingsRequest.getQueryParams().time_range ) {
+        console.log( savingsRequest.url );
+        const response = await fetch( savingsRequest.url );
         result = await response.json();
     }
 
