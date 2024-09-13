c8a3b55843b44abb1a2291984466697e6a57a277
Giannis Clipper <giannisclipper@gmail.com>
2024-09-13 13:11:00 +0300

    Improve the calculation of y ticks in charts.

--- src/logic/savings/chart/index.ts
+++ src/logic/savings/chart/index.ts
@@ -1,48 +1,62 @@
 import type { ObjectType } from '@/types';
 
-const _getYLimits = ( minValue: number, maxValue: number ): [ number, number ] => {
-
-    const log: number = Math.floor( Math.log10( ( maxValue - minValue ) / 10 ) );
-    const baseUnit: number = Math.pow( 10, log );
-    return [
-        Math.floor( minValue / baseUnit ) * baseUnit,
-        Math.ceil( maxValue / baseUnit ) * baseUnit
-    ]
-}
-
 const getYTicks = ( data: ObjectType[] ): number[] => {
 
     const minYValues = data.map( ( row: ObjectType ) => {
         const { time, quantity, quantities } = row;
+
+        // case of multiple quantities
         if ( quantities !== undefined ) {
             return Math.min( ...Object.values( quantities ).map( q => q.quantity ) );
         }
+
+        // case of single quantity
         return quantity;
     } );
 
     const maxYValues = data.map( ( row: ObjectType ) => {
         const { time, quantity, total, quantities } = row;
+
+        // case of multiple quantities including total (line chart)
         if ( total !== undefined ) {
             return total;
         }
+
+        // case of multiple quantities without total (area, bar charts)
         if ( quantities !== undefined ) {
             return Math.max( ...Object.values( quantities ).map( q => q.quantity ) );
         }
+
+        // case of single quantity
         return quantity;
     } );
 
-    const [ minYValue, maxYValue ] = _getYLimits( 
-        Math.min( ...minYValues ) * 1.10,
-        Math.max( ...maxYValues ) * 1.05
-    ); 
+    const minYValue: number = Math.min( ...minYValues ) * 0.90;
+    const maxYValue: number = Math.max( ...maxYValues ) * 1.05;
+    const diff: number = ( maxYValue - minYValue );
+    // console.log( 'minYValue, maxYValue, diff', minYValue, maxYValue, diff )
+    // for example: 0 1278834027 1278834027
 
-    const diff: number = ( maxYValue - minYValue ) * .5;
-    const log: number = Math.floor( Math.log10( diff ) );
-    const baseUnit: number = Math.pow( 10, log );
+    let log: number = Math.log10( diff );
+    const logDecimals: number = log - Math.trunc( log );
+    // console.log( 'log, logDecimals', log, logDecimals ) 
+    // for example: 9.10681418338768 0.10681418338768012
 
-    let value = minYValue > baseUnit ? minYValue : 0;
+    log = Math.trunc( log ) - ( logDecimals * 100000 < 69897 ? 1 : 0 );
+    log = log >= 1 ? log : 1;
+    // console.log( 'log', log ) 
+    // for example: 8
+
+    let baseUnit: number = Math.pow( 10, log );
+    let times = Math.ceil( Math.ceil( diff / baseUnit ) / 10 );
+    // console.log( 'baseUnit, times', baseUnit, times ) 
+    // for example: 100000000 2
+
+    baseUnit *= times;
+
+    let value = Math.floor( minYValue / baseUnit ) * baseUnit;
     const result = [ value ];
-    while ( value < maxYValue ) {
+    while ( value <= maxYValue ) {
         value += baseUnit;
         result.push( value );
     }
