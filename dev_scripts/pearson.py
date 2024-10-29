edd36e513d0b9d9c5a4aa644623aaa1b4fe60087
Giannis Clipper <giannisclipper@gmail.com>
2024-10-29 21:23:40 +0200

    Working on pearson, spearman, elbow, silhouette analysis.

---
+++ dev_scripts/pearson.py
@@ -0,0 +1,180 @@
+# to run script:
+# $ source ./venv/bin/activate
+# (venv) $ python -m dev_scripts/pearson
+
+import sys
+
+from dev_scripts.conninfo import conninfo
+import psycopg
+
+from scipy.stats import pearsonr, spearmanr
+
+import numpy as np
+import matplotlib.pyplot as plt
+import statistics
+
+# the calculation here is based on hydrologic years but it seem less declarative
+def run_pearson2():
+
+    query = """
+SELECT s.custom_year, s.quantity, w.precipitation_sum
+
+FROM (
+	SELECT s.custom_year, AVG( s.quantity ) quantity
+	FROM (
+		SELECT date, SUM( quantity ) AS quantity, EXTRACT( YEAR FROM date ) || '-' || EXTRACT( YEAR FROM date ) + 1 AS custom_year
+		FROM savings 
+		WHERE EXTRACT( MONTH FROM date ) >= 10 
+		GROUP BY date
+	UNION
+		SELECT date, SUM( quantity ) AS quantity, EXTRACT( YEAR FROM date ) -1 || '-' || EXTRACT( YEAR FROM date ) AS custom_year
+		FROM savings 
+		WHERE EXTRACT( MONTH FROM date ) < 10 
+		GROUP BY date
+	) s 
+	GROUP BY s.custom_year
+) s
+
+JOIN (
+	SELECT w.custom_year, SUM( w.precipitation_sum ) precipitation_sum
+	FROM (
+		SELECT date, SUM( precipitation_sum ) AS precipitation_sum, EXTRACT( YEAR FROM date ) || '-' || EXTRACT( YEAR FROM date ) + 1 AS custom_year
+		FROM weather 
+		WHERE EXTRACT( MONTH FROM date ) >= 10 
+		GROUP BY date
+	UNION
+		SELECT date, SUM( precipitation_sum ) AS precipitation_sum, EXTRACT( YEAR FROM date ) -1 || '-' || EXTRACT( YEAR FROM date ) AS custom_year
+		FROM weather 
+		WHERE EXTRACT( MONTH FROM date ) < 10 
+		GROUP BY date
+	) w
+	GROUP BY w.custom_year
+) w
+
+ON s.custom_year=w.custom_year 
+WHERE s.custom_year BETWEEN '1996-1997' AND '2023-2024'
+ORDER BY s.custom_year;
+    """
+
+    result = None
+    with psycopg.connect( conninfo=conninfo ) as conn, conn.cursor() as cur:
+        cur.execute( query )
+        result = cur.fetchall()
+        for row in result: print( row )
+
+    years = list( map( lambda r: r[ 0 ], result ) )
+    savings = np.array( list( map( lambda r: float( r[ 1 ] ), result ) ) )
+    precipitation = np.array( list( map( lambda r: float( r[ 2 ] ), result ) ) )
+
+    savings_norm01 = ( savings - np.min( savings ) ) / ( np.max( savings ) - np.min( savings ) )
+    precipitation_norm01 = ( precipitation - np.min( precipitation ) ) / ( np.max( precipitation ) - np.min( precipitation ) )
+
+    plt.figure( figsize=( 10, 6 ) )
+    # plt.suptitle( 'Αυξομείωση μεγεθών ως προς την προηγούμενη χρονιά', fontsize=14 )
+    plt.xlabel( 'Έτη', fontsize=12 )
+    # plt.ylabel("%", labelpad=12)
+    plt.tick_params( axis='x', labelrotation=90, labelsize=9 )
+    plt.tick_params( axis='y', labelrotation=0, labelsize=9 )
+    plt.plot( years[:], savings_norm01, label="μέσα ημερήσια αποθέματα νερού", color="deepskyblue" )
+    plt.plot( years[:], precipitation_norm01, label="συνολικός ετήσιος υετός", color="lightskyblue", linestyle='dashed' )
+    plt.legend() 
+    plt.grid()
+    plt.show()
+
+    # print( savings, precipitation )
+
+    print( pearsonr( savings, precipitation ) )
+    print( spearmanr( savings, precipitation ) )
+
+    print()
+
+    print( pearsonr( savings[ 1: ], precipitation[ :-1 ] ) )
+    print( spearmanr( savings[ 1: ], precipitation[ :-1 ] ) )
+
+def run_pearson():
+
+    query = """
+    SELECT s.year, s.savings, w.precipitation 
+
+    FROM (
+        SELECT SUBSTR(s.date::text,1,4) AS year, AVG(s.quantity) AS savings
+        FROM (
+            SELECT date, SUM(quantity) AS quantity 
+            FROM savings WHERE date >= '1996-01-01' AND date <= '2023-12-31'
+            GROUP BY date
+        ) s
+        GROUP BY SUBSTR(s.date::text,1,4)
+    ) s
+
+    JOIN (
+        SELECT SUBSTR(date::text,1,4) AS year, SUM(precipitation_sum) AS precipitation 
+        FROM weather WHERE date >= '1996-01-01' AND date <= '2023-12-31'
+        GROUP BY SUBSTR(date::text,1,4)
+    ) w
+
+    ON s.year=w.year
+    ORDER BY s.year;
+    """
+
+    result = None
+    with psycopg.connect( conninfo=conninfo ) as conn, conn.cursor() as cur:
+        cur.execute( query )
+        result = cur.fetchall()
+        for row in result: print( row )
+
+    years = list( map( lambda r: r[ 0 ], result ) )
+    savings = np.array( list( map( lambda r: float( r[ 1 ] ), result ) ) )
+    precipitation = np.array( list( map( lambda r: float( r[ 2 ] ), result ) ) )
+
+    # plot illustrating the hange of the values
+
+    savings_change = [ 0 ]
+    precipitation_change = [ 0 ]
+    for i in range( 1, len( years ) ):
+        savings_change.append( savings[ i ] / savings[ i -1 ] )
+        precipitation_change.append( precipitation[ i ] / precipitation[ i -1 ] )
+
+    plt.figure( figsize=( 10, 6 ) )
+    plt.suptitle( 'Μεταβολή στα αποθέματα νερού και τις μετρήσεις υετού', fontsize=14 )
+    plt.xlabel( 'Έτη', fontsize=12 )
+    plt.ylabel("Μεταβολή ως προς την προηγούμενη τιμή", labelpad=12)
+    plt.tick_params( axis='x', labelrotation=90, labelsize=9 )
+    plt.tick_params( axis='y', labelrotation=0, labelsize=9 )
+    plt.plot( years[:], savings_change, label="Μέση ημερήσια ποσότητα αποθεμάτων", color="deepskyblue" )
+    plt.plot( years[:], precipitation_change, label="Σύνολο ετήσιων μετρήσεων υετού", color="lightskyblue", linestyle='dashed' )
+    plt.legend() 
+    plt.grid()
+    # plt.show()
+
+    savings_norm01 = ( savings - np.min( savings ) ) / ( np.max( savings ) - np.min( savings ) )
+    precipitation_norm01 = ( precipitation - np.min( precipitation ) ) / ( np.max( precipitation ) - np.min( precipitation ) )
+
+    plt.figure( figsize=( 10, 6 ) )
+    plt.suptitle( 'Κανονικοποίηση τιμών στα αποθέματα νερού και τις μετρήσεις υετού', fontsize=14 )
+    plt.xlabel( 'Έτη', fontsize=12 )
+    plt.ylabel("Κανονικοποίηση από 0 έως 1", labelpad=12)
+    plt.tick_params( axis='x', labelrotation=90, labelsize=9 )
+    plt.tick_params( axis='y', labelrotation=0, labelsize=9 )
+    plt.plot( years[:], savings_norm01, label="Μέση ημερήσια ποσότητα αποθεμάτων", color="deepskyblue" )
+    plt.plot( years[:], precipitation_norm01, label="Ετήσιο σύνολο μετρήσεων υετού", color="lightskyblue", linestyle='dashed' )
+    plt.legend() 
+    plt.grid()
+    plt.show()
+
+    # print( savings, precipitation )
+
+    print( pearsonr( savings, precipitation ) )
+    print( spearmanr( savings, precipitation ) )
+
+    print()
+
+    print( pearsonr( savings[ 1: ], precipitation[ :-1 ] ) )
+    print( spearmanr( savings[ 1: ], precipitation[ :-1 ] ) )
+
+# help(pearsonr)
+# help(spearmanr)
+
+if __name__ == "__main__":
+    
+    run_pearson()
+    # run_pearson2()
---
+++ dev_scripts/silhouette.py
@@ -0,0 +1,274 @@
+# to run script:
+# $ source ./venv/bin/activate
+# (venv) $ python -m dev_scripts/shilouette
+
+import sys
+
+from dev_scripts.conninfo import conninfo
+import psycopg
+
+from sklearn.cluster import KMeans 
+from sklearn.metrics import silhouette_score
+MAX_ITER = 600
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+import statistics
+
+def run_boxplot( title, data ):
+    plt.boxplot( data )
+    plt.title( f'Box plot ({title})', fontsize=10 )
+    plt.show()
+
+def run_histogram( title, data ):
+    plt.hist( data, bins=10, color='lightblue' )
+    plt.title( f'Histogram ({title})', fontsize=10 )
+    plt.xlabel( 'Quantities' )
+    plt.ylabel( 'Years' )
+    plt.show()
+
+def run_normaldist( title, data ):
+
+    data.sort()
+    mean = statistics.mean( data )
+    sd = statistics.stdev( data )
+    mean_over_sd = round( mean / sd, 2 )
+    plt.plot( data, norm.pdf( data, mean, sd ), color='pink' )
+    plt.xlim( 0, 2 * mean )
+    # plt.hist( data, bins=10, color='lightblue' )
+    plt.suptitle( f'Normal distr. ({title})', fontsize=10 )
+    plt.title( f'mean: {int(mean)}, std dev: {round(sd,1)}, mean/sd: {mean_over_sd}', fontsize=8 )
+    plt.show()
+
+def run_elbow( title, data ):
+
+    no_of_clusters = [ 2, 3, 4, 5, 6, 7, 8, 9 ] 
+    sum_of_sq_distances = []
+    for n_clusters in no_of_clusters: 
+        kmeans = KMeans( n_clusters=n_clusters )
+        kmeans.fit( data )
+        sum_of_sq_distances.append( kmeans.inertia_ )
+
+    plt.plot( no_of_clusters, sum_of_sq_distances, marker='o')
+    plt.title( f'Elbow Curve method ({title})', fontsize=10 )
+    plt.xlabel( 'Number of clusters' )
+    plt.ylabel( 'Inertia (sum of sq. distances)' )
+    plt.show()
+
+def run_silhouette( title, data ):
+
+    no_of_clusters = [ 2, 3, 4, 5, 6, 7, 8, 9 ] 
+    shilouette_results = []
+    for n_clusters in no_of_clusters: 
+    
+        cluster = KMeans( n_clusters = n_clusters, max_iter=MAX_ITER, random_state=32 ) 
+        cluster_labels = cluster.fit_predict( data ) 
+    
+        # The silhouette_score gives the
+        # average value for all the samples. 
+        silhouette_avg = silhouette_score( data, cluster_labels ) 
+        shilouette_results.append( silhouette_avg )
+
+        print( "Number of clusters:", n_clusters, "Average silhouette score:", silhouette_avg )
+
+    plt.plot( no_of_clusters, shilouette_results, marker='o' )
+    plt.title( f'Shilouette Analysis ({title})', fontsize=10 )
+    plt.xlabel( 'Number of clusters' )
+    plt.ylabel( 'Silhouette score' )
+    plt.show()
+
+
+def savings_silhouette():
+
+    query = '''
+        SELECT SUBSTR( a.date::text,1,4) AS year, AVG( a.quantity ) AS quantity 
+        FROM (
+            SELECT date, SUM( quantity ) AS quantity 
+            FROM savings WHERE date < '2024-01-01' GROUP BY date
+        ) a
+        GROUP BY SUBSTR( a.date::text,1,4) 
+        ORDER BY SUBSTR( a.date::text,1,4);
+    '''
+
+    result = None
+    with psycopg.connect( conninfo=conninfo ) as conn, conn.cursor() as cur:
+        cur.execute( query )
+        result = cur.fetchall()
+        # for row in result: print( row )
+
+    data = list( map( lambda x: [ float( x[ 1 ] ) ], result ) )
+    # print( data, np.array( data ).reshape( -1 ) )
+
+    print( "Savings rows:", len( data ), "Max iterations:", MAX_ITER ) 
+
+    # run_boxplot( 'Αποθέματα νερού', np.array( data ).reshape( -1 ) )
+    run_histogram( 'Αποθέματα νερού', np.array( data ).reshape( -1 ) )
+    run_normaldist( 'Αποθέματα νερού', np.array( data ).reshape( -1 ) )
+    run_elbow( 'Αποθέματα νερού', data )
+    run_silhouette( 'Αποθέματα νερού', data )
+
+
+def production_silhouette():
+
+    query = '''
+        SELECT SUBSTR( a.date::text,1,4) AS year, SUM( a.quantity ) AS quantity 
+        FROM (
+            SELECT date, SUM( quantity ) AS quantity 
+            FROM production WHERE date < '2024-01-01' GROUP BY date
+        ) a
+        GROUP BY SUBSTR( a.date::text,1,4) 
+        ORDER BY SUBSTR( a.date::text,1,4);
+    '''
+
+    result = None
+    with psycopg.connect( conninfo=conninfo ) as conn, conn.cursor() as cur:
+        cur.execute( query )
+        result = cur.fetchall()
+        # for row in result: print( row )
+
+    data = list( map( lambda x: [ float( x[ 1 ] ) ], result ) )
+    print( data )
+
+    # print( "Production rows:", len( data ), "Max iterations:", MAX_ITER ) 
+
+    # run_boxplot( 'Παραγωγή νερού', np.array( data ).reshape( -1 ) )
+    run_histogram( 'Παραγωγή νερού', np.array( data ).reshape( -1 ) )
+    run_normaldist( 'Παραγωγή νερού', np.array( data ).reshape( -1 ) )
+    run_elbow( 'Παραγωγή νερό', data )
+    run_silhouette( 'Παραγωγή νερό', data )
+
+
+def precipitation_silhouette():
+
+    query = '''
+        SELECT SUBSTR( a.date::text,1,4) AS year, SUM( a.quantity ) AS quantity 
+        FROM (
+            SELECT date, SUM( precipitation_sum ) AS quantity 
+            FROM weather WHERE date < '2024-01-01' GROUP BY date
+        ) a
+        GROUP BY SUBSTR( a.date::text,1,4) 
+        ORDER BY SUBSTR( a.date::text,1,4);
+    '''
+
+    result = None
+    with psycopg.connect( conninfo=conninfo ) as conn, conn.cursor() as cur:
+        cur.execute( query )
+        result = cur.fetchall()
+        # for row in result: print( row )
+
+    data = list( map( lambda x: [ float( x[ 1 ] ) ], result ) )
+    print( data )
+
+    # print( "Precipitation rows:", len( data ), "Max iterations:", MAX_ITER ) 
+
+    # run_boxplot( 'Συνολικός υετός', np.array( data ).reshape( -1 ) )
+    run_histogram( 'Συνολικός υετός', np.array( data ).reshape( -1 ) )
+    run_normaldist( 'Συνολικός υετός', np.array( data ).reshape( -1 ) )
+    run_elbow( 'Συνολικός υετός', data )
+    run_silhouette( 'Συνολικός υετός', data )
+
+
+def temperature_silhouette():
+
+    query = '''
+        SELECT SUBSTR( a.date::text,1,4) AS year, AVG( a.temp_min ) AS temp_min, AVG( a.temp_mean ) AS temp_mean, AVG( a.temp_max ) AS temp_max 
+        FROM (
+            SELECT date, AVG( temperature_2m_min ) AS temp_min, AVG( temperature_2m_mean ) AS temp_mean, AVG( temperature_2m_max ) AS temp_max
+            FROM weather WHERE date < '2024-01-01' GROUP BY date
+        ) a
+        GROUP BY SUBSTR( a.date::text,1,4) 
+        ORDER BY SUBSTR( a.date::text,1,4);
+    '''
+
+    result = None
+    with psycopg.connect( conninfo=conninfo ) as conn, conn.cursor() as cur:
+        cur.execute( query )
+        result = cur.fetchall()
+        # for row in result: print( row )
+
+    # data = list( map( lambda x: [ float( x[ 1 ] ), float( x[ 2 ] ), float( x[ 3 ] ) ], result ) )
+    data = list( map( lambda x: [ float( x[ 2 ] ) ], result ) )
+    print( data )
+
+    print( "Temperature rows:", len( data ), "Max iterations:", MAX_ITER ) 
+
+    # run_boxplot( 'Μέση θερμοκρασία', np.array( data ).reshape( -1 ) )
+    run_histogram( 'Μέση θερμοκρασία', np.array( data ).reshape( -1 ) )
+    run_normaldist( 'Μέση θερμοκρασία', np.array( data ).reshape( -1 ) )
+    run_elbow( 'Μέση θερμοκρασία', data )
+    run_silhouette( 'Μέση θερμοκρασία', data )
+
+
+def interruptions_silhouette():
+
+    query = '''
+        SELECT a.municipality_id, a.events, b.area, a.events / b.area, b.population, a.events / ( 0.001 * b.population ) 
+        FROM (
+            SELECT municipality_id, COUNT( * ) AS events 
+            FROM interruptions GROUP BY municipality_id
+        ) a
+        JOIN municipalities b ON a.municipality_id=b.id;
+    '''
+
+    result = None
+    with psycopg.connect( conninfo=conninfo ) as conn, conn.cursor() as cur:
+        cur.execute( query )
+        result = cur.fetchall()
+        # for row in result: print( row )
+
+    # data = list( map( lambda x: [ float( x[ 3 ] ),  float( x[ 5 ] ) ], result ) )
+    data = list( map( lambda x: [ float( x[ 1 ] ) ], result ) )
+    print( data )
+
+    print( "Interruptions rows:", len( data ), "Max iterations:", MAX_ITER ) 
+
+    # run_boxplot( 'Διακοπές υδροδότησης', np.array( data ).reshape( -1 ) )
+    run_histogram( 'Διακοπές υδροδότησης', np.array( data ).reshape( -1 ) )
+    run_normaldist( 'Διακοπές υδροδότησης', np.array( data ).reshape( -1 ) )
+    run_elbow( 'Διακοπές υδροδότησης', data )
+    run_silhouette( 'Διακοπές υδροδότησης', data )
+
+    # import statistics
+    # events = sorted( list( map( lambda row: row[ 1 ], result ) ) )
+    # print( 'events', events )
+    # print( 'min', min( events ) )
+    # print( 'mean', statistics.mean( events ) )
+    # print( 'max', max( events ) )
+    # print( 'stdev', statistics.stdev( events ) )
+
+if __name__ == "__main__":
+
+    try:
+        if len( sys.argv ) < 2:
+            raise Exception( 'No dataset defined.' )
+
+        match sys.argv[ 1 ]:
+
+            case 'savings':
+                savings_silhouette()
+
+            case 'production':
+                production_silhouette()
+
+            case 'precipitation':
+                precipitation_silhouette()
+
+            case 'temperature':
+                temperature_silhouette()
+
+            case 'interruptions':
+                interruptions_silhouette()
+
+            case _:
+                raise Exception( 'No correct dataset.' )
+
+    except Exception as error:
+        print( 'Error: ' + repr( error ) )
+        print ( 'Syntax example: python -m dev_scripts/shilouette.py savings' )
+        print ( 'Syntax example: python -m dev_scripts/shilouette.py production' )
+        print ( 'Syntax example: python -m dev_scripts/shilouette.py precipitation' )
+        print ( 'Syntax example: python -m dev_scripts/shilouette.py temperature' )
+        print ( 'Syntax example: python -m dev_scripts/shilouette.py interruptions' )
+        exit( -1 )
+
